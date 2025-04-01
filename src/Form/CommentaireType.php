6761ce3b84ae15836d7e3bd6d535264397ae6333
mohamedDVP <mohamed.mak@laposte.net>
2025-04-01 20:27:22 +0200

    création des controleurs et des vues commentaire, film, genre, note et réalisateur et création des form  commentaire et note

---
+++ src/Form/CommentaireType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commentaire;
+use App\Entity\Film;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentaireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('description')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('publishedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('approuve')
+            ->add('film', EntityType::class, [
+                'class' => Film::class,
+                'choice_label' => 'id',
+            ])
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Commentaire::class,
+        ]);
+    }
+}
