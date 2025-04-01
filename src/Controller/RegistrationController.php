3a72175b568e25410d0c0852a8c846a1e741f876
mohamedDVP <mohamed.mak@laposte.net>
2025-04-01 21:11:11 +0200

    probleme avec passwordhasher()

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -8,28 +8,34 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\PasswordHasher\PasswordHasherInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 final class RegistrationController extends AbstractController
 {
 
     #[Route('/inscription', name: 'app_register')]
-    public function register(Request $request, EntityManagerInterface $em): Response
+    public function register(Request $request, EntityManagerInterface $em, PasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationType::class, $user);
         $form->handleRequest($request);
 
+        // Vérifier si le formulaire est soumis et valide
         if ($form->isSubmitted() && $form->isValid()) {
-            // Encoder le mot de passe
-            $hashedPassword = password_hash($user->getPassword(), PASSWORD_BCRYPT);
+            // Récupérer le mot de passe de l'utilisateur
+            $password = $form->get('password')->getData();
+            
+            // Encoder le mot de passe avec PasswordHasherInterface
+            $hashedPassword = $passwordHasher->hashPassword($user, $password);
             $user->setPassword($hashedPassword);
 
-            // Enregistrer l'utilisateur
+            // Enregistrer l'utilisateur en base de données
             $em->persist($user);
             $em->flush();
 
-            // Rediriger après l'inscription
+            // Rediriger vers la page de connexion ou une autre page
             $this->addFlash('success', 'Inscription réussie!');
             return $this->redirectToRoute('app_login');
         }
