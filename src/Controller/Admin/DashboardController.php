999a6cfcb89ef3ef1c89e169154a400799a7c718
mohamedDVP <mohamed.mak@laposte.net>
2025-04-06 13:59:40 +0200

    gestion de l'interface EasyAdmin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Film;
+use App\Entity\Note;
+use App\Entity\Genre;
+use App\Entity\Realisateur;
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -38,6 +42,13 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Films', 'fas fa-movie', Film::class);
+        yield MenuItem::linkToCrud('Films', 'fas fa-film', Film::class);
+        yield MenuItem::linkToCrud('Genre', 'fas fa-list', Genre::class);
+        yield MenuItem::linkToCrud('Commentaires', 'fas fa-comment', Genre::class);
+        yield MenuItem::linkToCrud('Note', 'fas fa-star', Note::class);
+        yield MenuItem::linkToCrud('Realisateur', 'fas fa-clapperboard', Realisateur::class);
+        yield MenuItem::linkToCrud('Utilisateur', 'fas fa-user', User::class);
+
+
     }
 }
