f25b48706e552f4b72b2b30990f137800f01cbd0
mohamedDVP <mohamed.mak@laposte.net>
2025-04-10 19:26:16 +0200

    gestion note en étoiles

--- src/Form/CommentaireNoteType.php
+++ src/Form/CommentaireNoteType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CommentaireNoteType extends AbstractType
@@ -22,17 +23,7 @@ class CommentaireNoteType extends AbstractType
                 'widget' => 'single_text',
             ])
             // Ajout du champ note : association avec l'entité Note
-            ->add('note', ChoiceType::class, [
-            'choices' => [
-                '1 étoile' => 1,
-                '2 étoiles' => 2,
-                '3 étoiles' => 3,
-                '4 étoiles' => 4,
-                '5 étoiles' => 5,
-            ],
-                'expanded' => true,
-                'multiple' => false,
-                'label' => 'Votre note (de 1 à 5)',
+            ->add('note', HiddenType::class, [
                 'mapped' => false,
             ])
             ->add('Envoyer', SubmitType::class, [
